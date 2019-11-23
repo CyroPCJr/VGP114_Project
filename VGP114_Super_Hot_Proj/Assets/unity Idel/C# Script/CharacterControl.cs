3656869fb42f4b611a697363ed9d5b366cea0404
KevinXu0427 <48079771+KevinXu0427@users.noreply.github.com>
2019-11-22 17:28:33 -0800

    Add Character and make it move, jump, rotate

---
+++ VGP114_Super_Hot_Proj/Assets/unity Idel/C# Script/CharacterControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    static Animator anim;
+    public float speed = 0.00005f;
+    public float rotationSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Jumping trigger when press space
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("isJumping");
+        }
+
+        // Moving
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isIdle", false);
+
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isIdle", true);
+        }
+
+    }
+}
