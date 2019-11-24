3221f6f8ba52eae972817f4a524901a66b5b6d32
Cyro Paulino da Costa Jr <cpcmetal@hotmail.com>
2019-11-23 20:56:00 -0800

    Make all the adjustment to make the movement  and also the enemies look at the player

--- VGP114_Super_Hot_Proj/Assets/Scripts/CharacterControl.cs
+++ VGP114_Super_Hot_Proj/Assets/Scripts/CharacterControl.cs
@@ -4,60 +4,37 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    //static Animator anim;
-    public float speed;
-    public float rotationSpeed;
-    public float normalSpeed;
+    private Vector3 mMovement = Vector3.zero;
+    private readonly float mSpeed = 10.0f;
+
+    private void MouseMovement()
+    {
+        //mMouseLookRot.y += Input.GetAxis("Mouse X");
+        //mMouseLookRot.x += -Input.GetAxis("Mouse Y");
+        //mMouseLookRot.x = Mathf.Clamp(mMouseLookRot.x, -15f, 15f);
+        //transform.eulerAngles = new Vector3(0, mMouseLookRot.y) * lookSpeed;
+        //transform.localRotation = Quaternion.Euler(mMouseLookRot.x * lookSpeed, 0, 0);
+        //transform.rotation = Quaternion.Euler(Input.GetAxis("Mouse X"), 0.0f,0.0f);
+    }
+
+    private void KeyboardMovement()
+    {
+        // Make movement foward and bacck, left and right
+        mMovement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.position += mMovement * mSpeed * Time.deltaTime;
+    }
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // Moving
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        rotation *= Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
-
-        if (translation != 0)
-        {
-            translation *= Time.deltaTime * normalSpeed;
-            
-            transform.Translate(0, 0, translation);
-           
-
-            // Jumping trigger when press space
-            //------- Running Jumping --------
-            //if (Input.GetKeyDown(KeyCode.Space))
-            //{
-            //    anim.SetTrigger("isRunJumping");
-            //}
-
-
-            //anim.SetBool("isRunning", true);
-            //anim.SetBool("isIdle", false);
-
-        }
-        else
-        {
-            // Jumping trigger when press space
-            //------- In place Jumping --------
-            //if (Input.GetKeyDown(KeyCode.Space))
-            //{
-            //    anim.SetTrigger("isJumping");
-            //}
-
-            //anim.SetBool("isRunning", false);
-            //anim.SetBool("isIdle", true);
-        }
-
+        MouseMovement();
+        KeyboardMovement();
     }
 }
--- VGP114_Super_Hot_Proj/Assets/Scripts/Enemy.cs
+++ VGP114_Super_Hot_Proj/Assets/Scripts/Enemy.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    public GameObject Player;
+    public GameObject Player = null;
     private readonly float mMinDistance = 15.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(Player.transform.position, transform.position) < mMinDistance)
+        if (Player && Vector3.Distance(Player.transform.position, transform.position) < mMinDistance)
         {
             Vector3 lookVector = Player.transform.position - transform.position;
+            //Debug.Log("D333333DD!!!");
+
+            //transform.forward = lookVector.normalized;
             lookVector.y = transform.position.y;
             Quaternion rot = Quaternion.LookRotation(lookVector);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+           transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        if (Player != null) {
+            if (Vector3.Distance(Player.transform.position, transform.position) < mMinDistance)
+            {
+                Gizmos.color = Color.red;
+            }
+            Gizmos.DrawLine(transform.position, Player.transform.position);
+        }
+
+        Vector3 position = transform.position;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(position, position + 3.0f * transform.forward);
     }
 }
