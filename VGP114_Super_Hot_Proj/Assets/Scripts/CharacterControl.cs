e3e07da5c5acee95b6fc11fb72cde4ee26f2311e
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2019-11-26 01:06:04 -0800

    Update Week 2
    - Update Enemy, add Attack;
    - Making some dummy test animation when attack;
    - Create Mouselook script attached to Player Object;
    - Update rotation when Enemy look at Player, rotation make more smooth.

--- VGP114_Super_Hot_Proj/Assets/Scripts/CharacterControl.cs
+++ VGP114_Super_Hot_Proj/Assets/Scripts/CharacterControl.cs
@@ -7,34 +7,11 @@ public class CharacterControl : MonoBehaviour
     private Vector3 mMovement = Vector3.zero;
     private readonly float mSpeed = 10.0f;
 
-    private void MouseMovement()
+    // Update is called once per frame
+    void Update()
     {
-        //mMouseLookRot.y += Input.GetAxis("Mouse X");
-        //mMouseLookRot.x += -Input.GetAxis("Mouse Y");
-        //mMouseLookRot.x = Mathf.Clamp(mMouseLookRot.x, -15f, 15f);
-        //transform.eulerAngles = new Vector3(0, mMouseLookRot.y) * lookSpeed;
-        //transform.localRotation = Quaternion.Euler(mMouseLookRot.x * lookSpeed, 0, 0);
-        //transform.rotation = Quaternion.Euler(Input.GetAxis("Mouse X"), 0.0f,0.0f);
-    }
-
-    private void KeyboardMovement()
-    {
-        // Make movement foward and bacck, left and right
         mMovement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         transform.position += mMovement * mSpeed * Time.deltaTime;
     }
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MouseMovement();
-        KeyboardMovement();
-    }
 }
--- VGP114_Super_Hot_Proj/Assets/Scripts/Enemy.cs
+++ VGP114_Super_Hot_Proj/Assets/Scripts/Enemy.cs
@@ -5,26 +5,67 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    public GameObject Player = null;
+    public Transform player;
     private readonly float mMinDistance = 15.0f;
+    private float mPlayerDistance = 0.0f;
+    private Animator mAnimator;
+
+#region Just for test
+    public int hitCount = 3; //number of hits
+    public float hitTime = 2.0f; //time in seconds between each hit
+    float curTime = 0; //time in seconds since last hit
+#endregion
+
 
     // Start is called before the first frame update
     void Start()
     {
+        mAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Player && Vector3.Distance(Player.transform.position, transform.position) < mMinDistance)
+        if (hitCount > 0) //if there are more hits left
         {
-            Vector3 lookVector = Player.transform.position - transform.position;
-            //Debug.Log("D333333DD!!!");
+            curTime += Time.deltaTime; //add time
+        }
+        mPlayerDistance = Vector3.Distance(player.transform.position, transform.position);
+        if (player && (mPlayerDistance < mMinDistance))
+        {
+            mAnimator.SetTrigger("isJumping");
+            LookAtPlayer();
+            if (mPlayerDistance <= 10.0f)
+            {
+                if (curTime >= hitTime)
+                {
+                    Attack();
+                }
+            }
+        }
+        else
+        {
+            mAnimator.SetBool("isIdle", true);
+        }
+    }
 
-            //transform.forward = lookVector.normalized;
-            lookVector.y = transform.position.y;
-            Quaternion rot = Quaternion.LookRotation(lookVector);
-           transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+    private void LookAtPlayer()
+    {
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2.0f);
+    }
+
+    private void Attack()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                //hit.collider.gameObject.GetComponent<>().health -= 5f;
+                Debug.Log("Enemy_Attack: Hit");
+                curTime = 0; //reset the time
+                hitCount--; //subtract one from the hit count
+            }
         }
     }
 
@@ -32,12 +73,13 @@ public class Enemy : MonoBehaviour
     {
         Gizmos.color = Color.green;
 
-        if (Player != null) {
-            if (Vector3.Distance(Player.transform.position, transform.position) < mMinDistance)
+        if (player)
+        {
+            if (mPlayerDistance < mMinDistance)
             {
                 Gizmos.color = Color.red;
             }
-            Gizmos.DrawLine(transform.position, Player.transform.position);
+            Gizmos.DrawLine(transform.position, player.transform.position);
         }
 
         Vector3 position = transform.position;
